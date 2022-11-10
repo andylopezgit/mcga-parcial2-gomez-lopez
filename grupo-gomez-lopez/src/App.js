3b32b464da99546fdcb163b0f5e2f2ef47e36802
ejecutorHarlan <54726387+ejecutorHarlan@users.noreply.github.com>
2022-11-10 18:26:48 -0300

    Styles on shared buttons and inputs for products view

--- grupo-gomez-lopez/src/App.js
+++ grupo-gomez-lopez/src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import LoginView from "./screems/LoginView/LoginView";
-import Layout from "./screems/LayoutView/Layout.";
-import AboutView from "./screems/AboutView/AboutView";
-
+import { Route, Routes } from 'react-router-dom';
+import LoginView from './screems/LoginView/LoginView';
+import Layout from './screems/LayoutView/Layout.';
+import AboutView from './screems/AboutView/AboutView';
+import ProductsView from './screems/ProductsView/ProductsView';
 
 function App() {
   return (
-      <Layout>
-          <Routes>
-              <Route path="/" element={<LoginView />} />
-              <Route path="/about" element={<AboutView />} />
-          </Routes>
-          <div className="App"></div>
-      </Layout>
-
+    <Layout>
+      <Routes>
+        <Route path='/' element={<LoginView />} />
+        <Route path='/about' element={<AboutView />} />
+        <Route path='/products' element={<ProductsView />} />
+      </Routes>
+      <div className='App'></div>
+    </Layout>
   );
 }
 

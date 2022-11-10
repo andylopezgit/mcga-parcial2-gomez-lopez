c878693ffe4df13b9793825d2fbaa7758766538c
andylopezgit <alopezciotta@gmail.com>
2022-11-10 18:20:05 -0300

    add types in redux: show_one_product, delete_product and update_product

--- grupo-gomez-lopez/src/redux/Product/actions.js
+++ grupo-gomez-lopez/src/redux/Product/actions.js
@@ -1,38 +1,41 @@
 import {
-    SHOW_PRODUCTS,
-    SET_PRODUCTS,
-    DISMISS_ISLOADING,
-    SET_ISLOADING,
+  SHOW_PRODUCTS,
+  SET_PRODUCTS,
+  SHOW_ONE_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+  SET_ISLOADING,
+  DISMISS_ISLOADING,
 } from "./types";
 
 export const setProducts = (products) => {
-    return {
-        type: SET_PRODUCTS,
-        payload: {
-            products: products,
-        },
-    };
+  return {
+    type: SET_PRODUCTS,
+    payload: {
+      products: products,
+    },
+  };
 };
 
 export const showProducts = (products) => {
-    return {
-        type: SHOW_PRODUCTS,
-        payload: {
-            products: products,
-        },
-    };
+  return {
+    type: SHOW_PRODUCTS,
+    payload: {
+      products: products,
+    },
+  };
 };
 
 export const setIsLoading = () => {
-    return {
-        type: SET_ISLOADING,
-        isLoadingProducts: true,
-    };
+  return {
+    type: SET_ISLOADING,
+    isLoadingProducts: true,
+  };
 };
 
 export const dismissIsLoading = () => {
-    return {
-        type: DISMISS_ISLOADING,
-        isLoadingProducts: false,
-    };
+  return {
+    type: DISMISS_ISLOADING,
+    isLoadingProducts: false,
+  };
 };
--- grupo-gomez-lopez/src/redux/Product/reducer.js
+++ grupo-gomez-lopez/src/redux/Product/reducer.js
@@ -1,41 +1,44 @@
 import {
-    SHOW_PRODUCTS,
-    SET_PRODUCTS,
-    SET_ISLOADING,
-    DISMISS_ISLOADING,
+  SHOW_PRODUCTS,
+  SET_PRODUCTS,
+  SHOW_ONE_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+  SET_ISLOADING,
+  DISMISS_ISLOADING,
 } from "./types";
 
 const INITIAL_STATE = {
-    isLoadingProducts: false,
-    product: [],
+  isLoadingProducts: false,
+  product: [],
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case SHOW_PRODUCTS:
-            return {
-                ...state,
-                product: action.payload,
-            };
-        case SET_PRODUCTS:
-            return {
-                ...state,
-                product: action.payload,
-            };
-        case SET_ISLOADING:
-            return {
-                ...state,
-                isLoadingProducts: true,
-            };
-        case DISMISS_ISLOADING:
-            return {
-                ...state,
-                isLoadingProducts: false,
-            };
+  switch (action.type) {
+    case SHOW_PRODUCTS:
+      return {
+        ...state,
+        product: action.payload,
+      };
+    case SET_PRODUCTS:
+      return {
+        ...state,
+        product: action.payload,
+      };
+    case SET_ISLOADING:
+      return {
+        ...state,
+        isLoadingProducts: true,
+      };
+    case DISMISS_ISLOADING:
+      return {
+        ...state,
+        isLoadingProducts: false,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default reducer;

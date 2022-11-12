f3b95822daf28fd44e27523ed8f5fae8acade080
ejecutorHarlan <54726387+ejecutorHarlan@users.noreply.github.com>
2022-11-12 11:26:34 -0300

    Uncomment form

--- grupo-gomez-lopez/src/screems/ProductsView/ProductsView.js
+++ grupo-gomez-lopez/src/screems/ProductsView/ProductsView.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useForm } from "react-hook-form";
-import InputShared from "../../components/sharedInput";
-import ButtonShared from "../../components/sharedButton";
-import { getProducts } from "../../redux/Product/thunks";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import InputShared from '../../components/sharedInput';
+import ButtonShared from '../../components/sharedButton';
+import { getProducts } from '../../redux/Product/thunks';
 
-import Styles from "../../constants/styles.module.css";
+import Styles from '../../constants/styles.module.css';
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -16,18 +16,18 @@ const Products = () => {
   }, [dispatch]);
   const { isLoadingProducts } = useSelector((state) => state.product);
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm();
-  // const onSubmit = (data) => console.log(data);
-  //
-  // const mail = watch("mail");
-  // console.log(mail);
-  // const pass = watch("pass");
-  // console.log(pass);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  //   const mail = watch("mail");
+  //   console.log(mail);
+  //   const pass = watch("pass");
+  //   console.log(pass);
   return (
     <>
       <table className={Styles.prodtable}>
@@ -35,7 +35,7 @@ const Products = () => {
           <tr>
             <th>ID</th>
             <th>Nombre</th>
-            <th>Descripcion</th>
+            <th>Stock</th>
             <th>Precio</th>
           </tr>
         </thead>
@@ -55,44 +55,40 @@ const Products = () => {
             })}
         </tbody>
       </table>
-      {/*<h1>Carga de productos</h1>*/}
-      {/*<form className={Styles.main} onSubmit={handleSubmit(onSubmit)}>*/}
-      {/*  <p>Ingrese sus datos de usuario y contraseña</p>*/}
-      {/*  <InputShared*/}
-      {/*    placeholder={"ID"}*/}
-      {/*    styleshare={Styles.inputbox}*/}
-      {/*    type={"text"}*/}
-      {/*    register={register}*/}
-      {/*    name={"id"}*/}
-      {/*  />*/}
-      {/*  <InputShared*/}
-      {/*    placeholder={"Nombre"}*/}
-      {/*    styleshare={Styles.inputbox}*/}
-      {/*    type={"text"}*/}
-      {/*    register={register}*/}
-      {/*    name={"name"}*/}
-      {/*  />*/}
-      {/*  <InputShared*/}
-      {/*    placeholder={"Descripción"}*/}
-      {/*    styleshare={Styles.inputbox}*/}
-      {/*    type={"text"}*/}
-      {/*    register={register}*/}
-      {/*    name={"description"}*/}
-      {/*  />*/}
-      {/*  <InputShared*/}
-      {/*    placeholder={"Precio"}*/}
-      {/*    styleshare={Styles.inputbox}*/}
-      {/*    type={"text"}*/}
-      {/*    register={register}*/}
-      {/*    name={"price"}*/}
-      {/*  />*/}
-      {/*  <ButtonShared*/}
-      {/*    text="Enviar"*/}
-      {/*    // Click={'/'}*/}
-      {/*    styleshare={Styles.buttonStyles}*/}
-      {/*    type={"submit"}*/}
-      {/*  />*/}
-      {/*</form>*/}
+
+      <h1>Carga de productos</h1>
+
+      <form className={Styles.main} onSubmit={handleSubmit(onSubmit)}>
+        <p>Ingrese los datos del producto</p>
+
+        <InputShared
+          placeholder={'Nombre'}
+          styleshare={Styles.inputbox}
+          type={'text'}
+          register={register}
+          name={'name'}
+        />
+        <InputShared
+          placeholder={'Stock'}
+          styleshare={Styles.inputbox}
+          type={'text'}
+          register={register}
+          name={'description'}
+        />
+        <InputShared
+          placeholder={'Precio'}
+          styleshare={Styles.inputbox}
+          type={'text'}
+          register={register}
+          name={'price'}
+        />
+        <ButtonShared
+          text='Enviar'
+          // Click={'/'}
+          styleshare={Styles.buttonStyles}
+          type={'submit'}
+        />
+      </form>
     </>
   );
 };

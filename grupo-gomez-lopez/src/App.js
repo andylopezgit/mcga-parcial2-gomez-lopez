adbbadcb2e5db330bce2ca9c4fc265ece1fa38e1
andylopezgit <alopezciotta@gmail.com>
2022-11-12 09:23:08 -0300

    add page not found in router when the route not match

--- grupo-gomez-lopez/src/App.js
+++ grupo-gomez-lopez/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import LoginView from './screems/LoginView/LoginView';
-import Layout from './screems/LayoutView/Layout.';
-import AboutView from './screems/AboutView/AboutView';
-import ProductsView from './screems/ProductsView/ProductsView';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LoginView from "./screems/LoginView/LoginView";
+import Layout from "./screems/LayoutView/Layout.";
+import AboutView from "./screems/AboutView/AboutView";
+import ProductsView from "./screems/ProductsView/ProductsView";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<LoginView />} />
-        <Route path='/about' element={<AboutView />} />
-        <Route path='/products' element={<ProductsView />} />
+        <Route path="/" element={<LoginView />} />
+        <Route path="/about" element={<AboutView />} />
+        <Route path="/products" element={<ProductsView />} />
+        **<Route path="*" element={<h1>Page-not-found-404</h1>}></Route>**
       </Routes>
-      <div className='App'></div>
+      <div className="App"></div>
     </Layout>
   );
 }
